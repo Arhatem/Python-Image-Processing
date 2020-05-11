deb47eb20873221744e03cde4557920c73a770c2
Hady <a.hadi.1997@gmail.com>
2020-05-11 03:36:46 +0200

    test_main more modifyed

--- test_main.py
+++ test_main.py
@@ -12,16 +12,24 @@ def get_string(img_path):
     # Read image with opencv
     img = cv2.imread(img_path)
     # Convert to gray
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Apply dilation and erosion to remove some noise
-    kernel = np.ones((1, 1), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   
+    # Blur
+    # img = cv2.medianBlur(img, 1)
+     
     #  Apply threshold to get image with only black and white
-    # img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    
+    _,img = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    # for i in range(4):
+    # Apply dilation and erosion to remove some noise   
+    kernel = np.ones((1, 1), np.uint8)    
+    # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    img =  cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    img = cv2.dilate(img, kernel, iterations=1)    
+    img = cv2.erode(img, kernel, iterations=1)
+    img = cv2.dilate(img, kernel, iterations=5)  
+    img = cv2.medianBlur(img,3)
+    cv2.imwrite('C:/Users/Hady/Desktop/img-processing-master/test.png', img)
     # Recognize text with tesseract for python
-    result = pytesseract.image_to_string(img)
+    result = pytesseract.image_to_string(img, lang='eng')
     return result
 
 class gui:
